f5b4b270de85af9bde528bf64083725841fcc3fa
Saputro-bit <muhammadnoorsaputro23@gmail.com>
2021-06-30 18:59:00 +0700

    FireBase2

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.firebase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.firebase.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button tambahbtn,lihatbtn;
